48f4a7efe54b91b42c0dffe05d9df03e4ab0b04a
Earbaj <earbajsaria3@gmail.com>
2021-12-04 21:44:30 +0600

    Create custome users class

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'auth_form.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
